9997893e3a33d7ccacd9488f7faf53cc7e32f0d2
Jeff Roden <roden80@gmail.com>
2023-11-15 20:43:24 -0600

    optiimum 36

--- 36.valid-sudoku.py
+++ 36.valid-sudoku.py
@@ -5,16 +5,34 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        squares = [set()] * 9
-        rows = [set()] * 9
-        columns = [set()] * 9
+        squares = [[set() for _ in range(3)] for _ in range(3)]
+        rows = [set() for _ in range(9)]
+        columns = [set() for _ in range(9)]
 
-        for row in range(9):
-            for col in range(9):
-                print(row, col)
+        for col in range(9):
+            for row in range(9):
+                if board[row][col] == '.':
+                    continue
 
+                if board[row][col] not in rows[row]:
+                    (rows[row]).add(board[row][col])
+                else:
+                    return False
+
+                if board[row][col] not in columns[col]:
+                    columns[col].add(board[row][col])
+                else:
+                    return False
+
+                if board[row][col] not in squares[row // 3][ col // 3]:
+                    (squares[row // 3][ col // 3]).add(board[row][col])
+                else:
+                    return False
+        return True
 
 board = [["5","3",".",".","7",".",".",".","."]
         ,["6",".",".","1","9","5",".",".","."]
