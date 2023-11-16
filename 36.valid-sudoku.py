0734a04581ba78973a710ca164b1b5b049e6df38
Jeff Roden <roden80@gmail.com>
2023-11-15 20:05:40 -0600

    start

---
+++ 36.valid-sudoku.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=36 lang=python3
+#
+# [36] Valid Sudoku
+#
+
+# @lc code=start
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        squares = [set()] * 9
+        rows = [set()] * 9
+        columns = [set()] * 9
+
+        for row in range(9):
+            for col in range(9):
+                print(row, col)
+
+
+board = [["5","3",".",".","7",".",".",".","."]
+        ,["6",".",".","1","9","5",".",".","."]
+        ,[".","9","8",".",".",".",".","6","."]
+        ,["8",".",".",".","6",".",".",".","3"]
+        ,["4",".",".","8",".","3",".",".","1"]
+        ,["7",".",".",".","2",".",".",".","6"]
+        ,[".","6",".",".",".",".","2","8","."]
+        ,[".",".",".","4","1","9",".",".","5"]
+        ,[".",".",".",".","8",".",".","7","9"]]
+
+Solution().isValidSudoku(board)
+# @lc code=end
+
