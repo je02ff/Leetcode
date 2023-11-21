21d9ae43b26587959482bdf9ab85a5d73211a0a9
Jeff Roden <roden80@gmail.com>
2023-11-20 18:46:34 -0600

    maybe

--- 15.3-sum.py
+++ 15.3-sum.py
@@ -7,6 +7,16 @@
 # @lc code=start
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        indexMap = {}
+        answerList = []
+        left = 0 
+        right = len(nums) - 1
+
+        while right > left:
+            sum = nums[left] + nums[right]
+            if  -1*sum in indexMap:
+                answerList.append([nums[left], nums[right], nums[-sum]])
         
+        return answerList
 # @lc code=end
 
