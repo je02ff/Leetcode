ba32b6c77cc27d9d921e09cbc1cd6641daf1448d
Jeff Roden <roden80@gmail.com>
2023-11-21 19:13:34 -0600

    brute force

--- 11.container-with-most-water.py
+++ 11.container-with-most-water.py
@@ -5,8 +5,21 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        
+        maxVolume = 0
+        for i in range(len(height)):
+            for j in range(i + 1, len(height)):
+                smallestHeight = min(height[i], height[j])
+                tempArea = smallestHeight * (j - i)
+
+                if maxVolume <  tempArea:
+                    maxVolume = tempArea
+        return maxVolume
+            
+
+            
 # @lc code=end
 
