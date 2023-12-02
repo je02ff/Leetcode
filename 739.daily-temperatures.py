935ea4ee97306f4e8f2179cc089bb55d3cd7b634
Jeff <roden80@gmail.com>
2023-12-02 15:09:03 -0600

    brute force - me

--- 739.daily-temperatures.py
+++ 739.daily-temperatures.py
@@ -9,6 +9,24 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = []
+
+        for i in range(len(temperatures)):
+            startTemp = temperatures[i]
+            for j in range(i, len(temperatures)):
+                currTemp = temperatures[j]
+                count = 0
+                if currTemp > startTemp:
+                    count = j - i
+                    break;
+            res.append(count)
+
         
+        return res
+
+Solution().dailyTemperatures([73,74,75,71,69,72,76,73])
+                
+
+
 # @lc code=end
 
