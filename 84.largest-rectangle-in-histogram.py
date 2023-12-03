122e81613397caa59826b21665f72ef23bc28a85
Jeff Roden <roden80@gmail.com>
2023-12-03 12:04:59 -0600

    begin

---
+++ 84.largest-rectangle-in-histogram.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=84 lang=python3
+#
+# [84] Largest Rectangle in Histogram
+#
+
+# @lc code=start
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        
+# @lc code=end
+
