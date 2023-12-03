dcdf770859a1cd9c2341a08573cda1fab487d236
Jeff Roden <roden80@gmail.com>
2023-12-02 18:15:56 -0600

    neetcode

--- Stack/739.daily-temperatures.py
+++ Stack/739.daily-temperatures.py
@@ -9,18 +9,14 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        res = []
+        res = [0] * len(temperatures)
+        stack = []
 
-        for i in range(len(temperatures)):
-            startTemp = temperatures[i]
-            for j in range(i, len(temperatures)):
-                currTemp = temperatures[j]
-                count = 0
-                if currTemp > startTemp:
-                    count = j - i
-                    break;
-            res.append(count)
-
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = (i - stackInd)
+            stack.append([t, i])
         
         return res
 
