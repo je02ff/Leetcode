e51cc0611005eeedbd0bd86667b013bb5372c894
Jeff Roden <roden80@gmail.com>
2023-12-03 21:07:42 -0600

    recursive - me

--- Binary Search/704.binary-search.py
+++ Binary Search/704.binary-search.py
@@ -5,8 +5,32 @@
 #
 
 # @lc code=start
+from typing import List
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        def scan(left: int, right: int, nums: List[int], target: int):
+            mid = ((right - left) // 2) + left
+            
+            if right < left:
+                return -1
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[mid] > target:
+                return scan(left,mid - 1,nums, target)
+            else:
+                return scan(mid + 1,right,nums, target)
         
+        left = 0
+        right = len(nums) - 1
+
+        return scan(left, right, nums, target)
+
+
+Solution().search([-1,0,3,5,9,12], 2)
+
 # @lc code=end
 
