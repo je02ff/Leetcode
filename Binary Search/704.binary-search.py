012323b128a4480a658b657bf43f12a10dc5fe6b
Jeff Roden <roden80@gmail.com>
2023-12-03 21:14:52 -0600

    iterative - me

--- Binary Search/704.binary-search.py
+++ Binary Search/704.binary-search.py
@@ -10,27 +10,27 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def scan(left: int, right: int, nums: List[int], target: int):
-            mid = ((right - left) // 2) + left
-            
-            if right < left:
-                return -1
-
-            if nums[mid] == target:
-                return mid
-
-            if nums[mid] > target:
-                return scan(left,mid - 1,nums, target)
-            else:
-                return scan(mid + 1,right,nums, target)
-        
         left = 0
         right = len(nums) - 1
+        
+        while right >= left:
+            mid = ((right - left) // 2) + left
+            
+            if nums[mid] == target:
+                return mid
+            
+            if nums[mid] > target:
+                right = mid - 1
 
-        return scan(left, right, nums, target)
+            if nums[mid] < target:
+                left = mid + 1
 
 
-Solution().search([-1,0,3,5,9,12], 2)
+
+        return -1
+
+
+Solution().search([5], 5)
 
 # @lc code=end
 
