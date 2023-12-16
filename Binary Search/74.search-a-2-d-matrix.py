75a0bcc1be6584a05fc87630404469d005dea3c0
Jeff <roden80@gmail.com>
2023-12-16 16:06:15 -0600

    comments

--- Binary Search/74.search-a-2-d-matrix.py
+++ Binary Search/74.search-a-2-d-matrix.py
@@ -10,6 +10,13 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
+
+
+# 1. get middle row
+# 2. is first element of middle row the number?
+# 3. is first element of middle row higher or lower than target?
+# 4. if higher than target -> first row is mid row + 1
+#    if lower than target -> 
+#    else -> bottom row is mid row - 1
 # @lc code=end
 
