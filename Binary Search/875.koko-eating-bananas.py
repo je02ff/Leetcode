a2a212dd42490a9560862eff226dae46092141fc
Jeff Roden <roden80@gmail.com>
2023-12-17 17:27:35 -0600

    o(log(max(piles) * piles))

--- Binary Search/875.koko-eating-bananas.py
+++ Binary Search/875.koko-eating-bananas.py
@@ -5,8 +5,30 @@
 #
 
 # @lc code=start
+import math
+import sys
+from typing import List
+
+
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        
-# @lc code=end
+        optimalBananaEating = max(piles)
+        left, right = 1, optimalBananaEating
 
+        while right >= left:
+            eatingSpeed = (right + left) // 2
+
+            eatingHours = 0
+            for bananas in piles:
+                eatingHours += math.ceil(bananas / eatingSpeed)
+            
+            if eatingHours <= h:
+                optimalBananaEating = min(eatingSpeed, optimalBananaEating)
+                right = eatingSpeed - 1
+            else:
+                left = eatingSpeed + 1
+
+        return optimalBananaEating
+    
+Solution().minEatingSpeed([3,6,7,11], 8)
+# @lc code=end
