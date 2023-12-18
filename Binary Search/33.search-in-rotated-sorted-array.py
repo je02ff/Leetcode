a1faeaf701f0b92acb59cb3ae2bb4bb2f3f68359
Jeff Roden <roden80@gmail.com>
2023-12-17 20:02:13 -0600

    begin

---
+++ Binary Search/33.search-in-rotated-sorted-array.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=33 lang=python3
+#
+# [33] Search in Rotated Sorted Array
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+# @lc code=end
